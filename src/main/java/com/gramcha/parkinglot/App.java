2dfdd13263744807ef4ce0cb3a78b81569171483
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2019-02-03 02:16:49 +0530

    Update main app class to handle file or interactive cmd line

--- src/main/java/com/gramcha/parkinglot/App.java
+++ src/main/java/com/gramcha/parkinglot/App.java
@@ -1,13 +1,21 @@
 package com.gramcha.parkinglot;
 
+import com.gramcha.parkinglot.service.CommandProcessorService;
+import com.gramcha.parkinglot.service.impl.CmdLineProcessorService;
+import com.gramcha.parkinglot.service.impl.FileProcessorService;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		CommandProcessorService cmdService;
+		if (args.length > 0) {
+			cmdService = new FileProcessorService(args[0]);
+		} else {
+			cmdService = new CmdLineProcessorService();
+		}
+		cmdService.parse();
+	}
 }

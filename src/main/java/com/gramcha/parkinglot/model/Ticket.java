7a6a327054c6d23d7dbd49ee98b526d28d190d19
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2019-02-02 14:37:21 +0530

    refactoring: adding boolean var to represents the allotted status.

--- src/main/java/com/gramcha/parkinglot/model/Ticket.java
+++ src/main/java/com/gramcha/parkinglot/model/Ticket.java
@@ -4,25 +4,36 @@ public class Ticket {
 	private String registrationNumber;
 	private String color;
 	private int allottedSlot;
-	public Ticket(String registrationNumber, String color, int allottedSlot) {
+	boolean isAlloted;
+
+	public Ticket(String registrationNumber, String color, int allottedSlot, boolean isAlloted) {
 		this.registrationNumber = registrationNumber;
 		this.color = color;
 		this.allottedSlot = allottedSlot;
+		this.isAlloted = isAlloted;
 	}
+
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public int getAllottedSlot() {
 		return allottedSlot;
 	}
+
+	public boolean isAlloted() {
+		return isAlloted;
+	}
+
 	public String toString() {
-		if(allottedSlot == -1) {
+		if (allottedSlot == -1) {
 			return "Sorry, parking lot is full";
 		} else {
-			return "Allocated slot number: "+allottedSlot;
+			return "Allocated slot number: " + allottedSlot;
 		}
 	}
 }
